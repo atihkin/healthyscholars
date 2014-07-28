b929000e71703cee50a5b49e4805c918b4d3e28d
Nikitha Suryadevara <praveen@zemosolabs.com>
2014-07-28 17:45:08 +0530

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Healthyscholars::Application.config.secret_key_base = '608c4795e7ae95a752f60531a4292ddf259a2c8f7cb4addf6e8cdb44d7d6ccaa0760ac3ada8c14e231382103da2c4464f262fa7da054ba1c99c39a247a15f08f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Healthyscholars::Application.config.session_store :cookie_store, key: '_healthyscholars_session'
